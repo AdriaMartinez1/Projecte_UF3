2acc813fff3f3d63c06a0f15f28c34bd8a9ef57e
AdriaMartinez1 <120479490+AdriaMartinez1@users.noreply.github.com>
2025-01-28 20:19:16 +0100

    Commit 4

---
+++ activitat/src/main/java/adria/activitat/ConnectionManager.java
@@ -0,0 +1,45 @@
+package adria.activitat;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class ConnectionManager {
+
+    public ConnectionManager(String databaseName, String collectionName, String nomUsuariMongo, String passwordMongo,
+    String uRLMongo) {
+super();
+    this.databaseName = databaseName;
+    this.collectionName = collectionName;
+    this.nomUsuariMongo = nomUsuariMongo;
+    this.passwordMongo = passwordMongo;
+    URLMongo = uRLMongo;
+    }
+
+    static String databaseName = "";
+    static String collectionName = "";
+    static String nomUsuariMongo = "";
+    static String passwordMongo = "";
+    static String URLMongo = "";
+    static MongoCollection<Document> collection;
+        
+    
+        public MongoDatabase getConnection()
+        {
+        String uri = "mongodb+srv://"+nomUsuariMongo+":"+passwordMongo+"@"+URLMongo+"/";
+	    MongoClient mongoClient = MongoClients.create(uri);
+
+            // Obtener o crear una base de datos
+
+			MongoDatabase database = mongoClient.getDatabase(databaseName); // <-- Nom de la base de dades
+            // Obtener o crear una colección
+			collection = database.getCollection(collectionName); // <-- Nom de la col.lecció
+                        return database;
+
+		
+    }
+
+}
--- activitat/src/main/java/adria/activitat/Main.java
+++ activitat/src/main/java/adria/activitat/Main.java
@@ -1,13 +1,38 @@
 package adria.activitat;
 
+import java.util.Scanner;
+
+import com.mongodb.client.MongoDatabase;
+
 /**
  * Hello world!
  *
  */
 public class Main 
 {
+	
+	String databaseName = "BookDatabase";
+    String collectionName = "Data";
+    String nomUsuariMongo = "Adria";
+	String passwordMongo = "exercici1";
+	String URLMongo = "cluster0.hlg7r.mongodb.net";
+    MongoDatabase mongoconnection = null;
+
+
+	ConnectionManager mongomanager = null;
+    
+    
+    public void start()
+    {
+        mongomanager = new ConnectionManager(databaseName, collectionName, nomUsuariMongo, passwordMongo, URLMongo);
+        mongoconnection = mongomanager.getConnection();
+ 
+        
+    }
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Main program = new Main();
+        program.start();
     }
 }
